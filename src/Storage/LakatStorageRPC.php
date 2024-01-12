6d1d1fc89dc60af3abf3a5b8808f1278a3dc47a2
Kuzma Kudim <kuzmaka@gmail.com>
2024-01-12 17:17:40 +0200

    wikimedia style code formatting

--- src/Storage/LakatStorageRPC.php
+++ src/Storage/LakatStorageRPC.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extension\Lakat\Storage;
 
+use Exception;
+use LogicException;
 use MediaWiki\Http\HttpRequestFactory;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
@@ -9,8 +11,7 @@ use Psr\Log\LoggerAwareTrait;
 use Status;
 use Wikimedia\UUID\GlobalIdGenerator;
 
-class LakatStorageRPC implements LakatStorageInterface
-{
+class LakatStorageRPC implements LakatStorageInterface {
 	use LoggerAwareTrait;
 
 	private static LakatStorageRPC $instance;
@@ -20,7 +21,7 @@ class LakatStorageRPC implements LakatStorageInterface
 	private GlobalIdGenerator $globalIdGenerator;
 	private HttpRequestFactory $httpRequestFactory;
 
-	public static function getInstance(): LakatStorageRPC {
+	public static function getInstance() : LakatStorageRPC {
 		if ( !isset( self::$instance ) ) {
 			self::$instance = new self;
 		}
@@ -28,9 +29,8 @@ class LakatStorageRPC implements LakatStorageInterface
 		return self::$instance;
 	}
 
-	public function __construct()
-	{
-		$this->setLogger(LoggerFactory::getInstance('lakat-rpc'));
+	public function __construct() {
+		$this->setLogger( LoggerFactory::getInstance( 'lakat-rpc' ) );
 
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'lakat' );
 		$this->url = $config->get( 'LakatRpcUrl' );
@@ -39,14 +39,7 @@ class LakatStorageRPC implements LakatStorageInterface
 		$this->httpRequestFactory = MediaWikiServices::getInstance()->getHttpRequestFactory();
 	}
 
-	public function createGenesisBranch(
-		int $branchType,
-		string $name,
-		string $signature,
-		bool $acceptConflicts,
-		string $msg
-	): string
-    {
+	public function createGenesisBranch( int $branchType, string $name, string $signature, bool $acceptConflicts, string $msg ) : string {
 		$method = 'create_genesis_branch';
 		$params = [
 			'branch_type' => $branchType,
@@ -56,41 +49,35 @@ class LakatStorageRPC implements LakatStorageInterface
 			'msg' => $msg,
 		];
 
-		return $this->rpc($method, array_values($params));
+		return $this->rpc( $method, array_values( $params ) );
 	}
 
-    public function branches(): array
-    {
-		throw new \LogicException('Not implemented');
-    }
-
-	public function submitFirst(string $branchId, string $articleName, string $content): string
-    {
-		throw new \LogicException('Not implemented');
-    }
-
-    public function submitNext(string $branchId, string $articleId, string $content): void
-    {
-		throw new \LogicException('Not implemented');
-    }
-
-    public function fetchArticle(string $branchId, string $articleId): string
-    {
-		throw new \LogicException('Not implemented');
-    }
-
-	public function findArticleIdByName(string $branchId, string $articleName): ?string
-	{
-		throw new \LogicException('Not implemented');
+	public function branches() : array {
+		throw new LogicException( 'Not implemented' );
 	}
 
-	private function rpc( string $method, array $params )
-	{
+	public function submitFirst( string $branchId, string $articleName, string $content ) : string {
+		throw new LogicException( 'Not implemented' );
+	}
+
+	public function submitNext( string $branchId, string $articleId, string $content ) : void {
+		throw new LogicException( 'Not implemented' );
+	}
+
+	public function fetchArticle( string $branchId, string $articleId ) : string {
+		throw new LogicException( 'Not implemented' );
+	}
+
+	public function findArticleIdByName( string $branchId, string $articleName ) : ?string {
+		throw new LogicException( 'Not implemented' );
+	}
+
+	private function rpc( string $method, array $params ) {
 		$data = [
 			'jsonrpc' => '2.0',
-			'id'      => $this->globalIdGenerator->newRawUUIDv4(),
-			'method'  => $method,
-			'params'  => $params,
+			'id' => $this->globalIdGenerator->newRawUUIDv4(),
+			'method' => $method,
+			'params' => $params,
 		];
 		$options = [
 			'method' => 'POST',
@@ -103,18 +90,14 @@ class LakatStorageRPC implements LakatStorageInterface
 			$errors = $status->getErrorsByType( 'error' );
 			$this->logger->error(
 				serialize( Status::wrap( $status )->getMessage( false, false, 'en' ) ),
-				[
-					'error' => $errors,
-					'caller' => __METHOD__,
-					'content' => $request->getContent()
-				]
+				[ 'error' => $errors, 'caller' => __METHOD__, 'content' => $request->getContent() ]
 			);
-			throw new \Exception('RPC HTTP call failed: ' . var_export($errors, 1));
+			throw new Exception( 'RPC HTTP call failed: ' . var_export( $errors, 1 ) );
 		}
 
 		$jsonResponse = $request->getContent();
 		$response = json_decode( $jsonResponse, true, 512, JSON_THROW_ON_ERROR );
-		if (isset($response["error"]) || !isset($response['result'])) {
+		if ( isset( $response["error"] ) || !isset( $response['result'] ) ) {
 			// Sample error response:
 			//{
 			//  "error": {
@@ -131,7 +114,7 @@ class LakatStorageRPC implements LakatStorageInterface
 			//  "id": "d7f316bc075f4933912566ea9bd591ef",
 			//  "jsonrpc": "2.0"
 			//}
-			throw new \Exception('RPC call failed: ' . $jsonResponse);
+			throw new Exception( 'RPC call failed: ' . $jsonResponse );
 		}
 
 		// Sample success response:
