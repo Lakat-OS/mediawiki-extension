fe5604c8f7fef74130f9db99be49809eb6e7b0f6
Kuzma Kudim <kuzmaka@gmail.com>
2024-01-12 17:01:46 +0200

    request/response format hints in comments

--- src/Storage/LakatStorageRPC.php
+++ src/Storage/LakatStorageRPC.php
@@ -115,6 +115,7 @@ class LakatStorageRPC implements LakatStorageInterface
 		$jsonResponse = $request->getContent();
 		$response = json_decode( $jsonResponse, true, 512, JSON_THROW_ON_ERROR );
 		if (isset($response["error"]) || !isset($response['result'])) {
+			// Sample error response:
 			//{
 			//  "error": {
 			//    "code": -32602,
@@ -133,6 +134,8 @@ class LakatStorageRPC implements LakatStorageInterface
 			throw new \Exception('RPC call failed: ' . $jsonResponse);
 		}
 
+		// Sample success response:
+		// {"result": "AVESAmkJ", "id": "4fb833ab66314861ae081a688bb1ac18", "jsonrpc": "2.0"}
 		return $response['result'];
 	}
 }
