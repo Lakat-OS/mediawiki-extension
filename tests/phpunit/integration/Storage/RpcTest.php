3e8acdeacbc4363395cd49b3ba0032e274183845
Kuzma Kudim <kuzmaka@gmail.com>
2024-01-17 18:20:31 +0200

    added RPC test file

---
+++ tests/phpunit/integration/Storage/RpcTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\Extension\Lakat\Domain\BranchType;
+use MediaWiki\Extension\Lakat\Storage\LakatStorageRPC;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\MediaWikiServices;
+use Psr\Log\NullLogger;
+
+/**
+ * @group rpc
+ * @coversDefaultClass LakatStorageRPC
+ */
+class RpcTest extends MediaWikiIntegrationTestCase {
+	protected function setUp() : void {
+		parent::setUp();
+
+		// use HttpRequestFactory instead of NullHttpRequestFactory to test RPC calls
+		$services = MediaWikiServices::getInstance();
+		$services->resetServiceForTesting('HttpRequestFactory');
+		$services->redefineService(
+			'HttpRequestFactory',
+			static function ( MediaWikiServices $services ) {
+				return new HttpRequestFactory(
+					new ServiceOptions( HttpRequestFactory::CONSTRUCTOR_OPTIONS, $services->getMainConfig() ),
+					new NullLogger()
+				);
+			}
+		);
+	}
+
+	/**
+	 * @covers ::createGenesisBranch
+	 */
+	public function testCreateGenesisBranch() : void {
+		$branchType = BranchType::TWIG;
+		$name = 'Test branch';
+		$signature = "\x00\x10\x20\x30";
+		$acceptConflicts = true;
+		$msg = 'Create genesis test branch';
+		$branchId = LakatStorageRPC::getInstance()->createGenesisBranch( $branchType, $name, $signature, $acceptConflicts, $msg );
+		$this->assertNotEmpty($branchId);
+	}
+}
