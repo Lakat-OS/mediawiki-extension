11ba5ee7d518a8045474b2fde87cd1e844e45736
Kuzma Kudim <kuzmaka@gmail.com>
2024-01-31 20:19:04 +0200

    staging: checkboxes to select articles for submit

--- src/Special/SpecialStaging.php
+++ src/Special/SpecialStaging.php
@@ -11,6 +11,10 @@ use Status;
 class SpecialStaging extends FormSpecialPage {
 	private StagingService $stagingService;
 
+	private string $branchName;
+
+	private array $stagedArticles;
+
 	public function __construct( StagingService $stagingService ) {
 		parent::__construct( 'Staging' );
 
@@ -30,7 +34,20 @@ class SpecialStaging extends FormSpecialPage {
 	}
 
 	protected function getFormFields() {
+		$branchName = $this->getBranchName();
+		if ( !$branchName ) {
+			return [];
+		}
+
+		$articles = $this->getStagedArticles( $branchName );
+		$options = array_combine($articles, $articles);
+
 		return [
+			'articles' => [
+				'type' => 'multiselect',
+				'label-message' => 'staging-modified-articles',
+				'options' => $options
+			],
 			'branch' => [
 				'type' => 'text',
 				'label-message' => 'staging-branch',
@@ -44,50 +61,46 @@ class SpecialStaging extends FormSpecialPage {
 		];
 	}
 
-	/**
-	 * List staged articles
-	 */
-	protected function postHtml() {
-		$html = Html::element( 'h2', [], $this->msg('staging-modified-articles')->escaped() );
-
+	protected function preHtml() {
 		$branchName = $this->getBranchName();
 		if ( !$branchName ) {
-			$html .= Html::errorBox( $this->msg( 'staging-branch-not-specified' )->escaped() );
-
-			return $html;
+			return Html::errorBox( $this->msg( 'staging-branch-not-specified' )->escaped() );
 		}
 
-		$articles = $this->stagingService->getStagedArticles( $branchName );
+		$articles = $this->getStagedArticles( $branchName );
 		if ( !$articles ) {
-			$html .= Html::noticeBox( $this->msg( 'staging-nothing-staged' )->escaped(), '' );
-
-			return $html;
+			return Html::noticeBox( $this->msg( 'staging-nothing-staged' )->escaped(), '' );
 		}
 
-		$html .= Html::openElement( 'ul' );
-		foreach ( $articles as $article ) {
-			$html .= Html::element( 'li', [], $article );
-		}
-		$html .= Html::closeElement( 'ul' );
-
-		return $html;
+		return '';
 	}
 
 	public function onSubmit( array $data ) {
+		$articles = $data['articles'];
 		$branch = $data['branch'];
 		$message = $data['message'];
-		$this->stagingService->submitStaged( $this->getUser(), $branch, $message );
+		$this->stagingService->submitStaged( $this->getUser(), $branch, $articles, $message );
 
 		return Status::newGood();
 	}
 
 	private function getBranchName(): string {
-		return $this->par ?: $this->getDefaultBranch();
+		if (!isset($this->branchName)) {
+			$this->branchName = $this->par ?: $this->getDefaultBranch();
+		}
+		return $this->branchName;
 	}
 
 	private function getDefaultBranch(): string {
 		$user = $this->getUser();
 		$userOptionsManager = MediaWikiServices::getInstance()->getUserOptionsManager();
 		return $userOptionsManager->getOption( $user, 'lakat-default-branch');
+	}
+
+	public function getStagedArticles( string $branchName ): array {
+		if (!isset($this->stagedArticles)) {
+			$this->stagedArticles = $this->stagingService->getStagedArticles( $branchName );
+		}
+		return $this->stagedArticles;
 	}
 }
