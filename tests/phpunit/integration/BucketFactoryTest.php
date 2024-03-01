9413021b4d4f10bac94b50eca0dfb6a63399b825
Kuzma Kudim <kuzmaka@gmail.com>
2024-03-01 20:09:55 +0200

    BucketFactoryTest: fix refs test

--- tests/phpunit/integration/BucketFactoryTest.php
+++ tests/phpunit/integration/BucketFactoryTest.php
@@ -5,17 +5,23 @@ namespace MediaWiki\Extension\Lakat\Tests\Integration;
 use MediaWiki\Extension\Lakat\Domain\AtomicBucket;
 use MediaWiki\Extension\Lakat\Domain\BucketFactory;
 use MediaWiki\Extension\Lakat\Domain\MolecularBucket;
+use MediaWiki\Page\WikiPageFactory;
+use MediaWiki\Title\Title;
 use MediaWikiIntegrationTestCase;
 
 /**
+ * @group Database
  * @coversDefaultClass \MediaWiki\Extension\Lakat\Domain\BucketFactory
  */
 class BucketFactoryTest extends MediaWikiIntegrationTestCase {
+	private WikiPageFactory $wikiPageFactory;
+
 	private BucketFactory $bucketFactory;
 
 	protected function setUp(): void {
 		parent::setUp();
 
+		$this->wikiPageFactory = $this->getServiceContainer()->getWikiPageFactory();
 		$this->bucketFactory = $this->getServiceContainer()->get(BucketFactory::SERVICE_NAME);
 	}
 
@@ -46,9 +52,12 @@ class BucketFactoryTest extends MediaWikiIntegrationTestCase {
 	 * @covers ::fromWikiText
 	 */
 	public function testArticleFromWikiText_ref() {
+		$this->assertTrue($this->editPage('Section1', 'Content1')->isGood());
+		$this->assertTrue($this->editPage('Section2', 'Content2')->isGood());
+
 		$article = $this->bucketFactory->fromWikiText(
 			'Test article',
-			'aaa{{:Article1}}bbb{{:Article2}}ccc'
+			'aaa{{:Section1}}bbb{{:Section2}}ccc'
 		);
 
 		$buckets = $article->buckets();
@@ -56,12 +65,29 @@ class BucketFactoryTest extends MediaWikiIntegrationTestCase {
 		$this->assertInstanceOf( AtomicBucket::class, $buckets[0] );
 		$this->assertEquals( 'aaa', $buckets[0]->data() );
 		$this->assertInstanceOf( MolecularBucket::class, $buckets[1] );
-		$this->assertEquals( '{{:Article1}}', $buckets[1]->ref() );
+		$this->assertEquals( 'Section1', $buckets[1]->name() );
 		$this->assertInstanceOf( AtomicBucket::class, $buckets[2] );
 		$this->assertEquals( 'bbb', $buckets[2]->data() );
 		$this->assertInstanceOf( MolecularBucket::class, $buckets[3] );
-		$this->assertEquals( '{{:Article2}}', $buckets[3]->ref() );
+		$this->assertEquals( 'Section2', $buckets[3]->name() );
 		$this->assertInstanceOf( AtomicBucket::class, $buckets[4] );
 		$this->assertEquals( 'ccc', $buckets[4]->data() );
+	}
+
+	/**
+	 * @covers ::fromWikiPage
+	 */
+	public function testArticleFromWikiPage() {
+		$name = 'Article';
+		$this->assertTrue( $this->editPage( $name, 'Content' )->isGood() );
+
+		$title = Title::newFromText( $name );
+		$page = $this->wikiPageFactory->newFromTitle( $title );
+		$article = $this->bucketFactory->fromWikiPage( $page );
+
+		$buckets = $article->buckets();
+		$this->assertCount( 1, $buckets );
+		$this->assertInstanceOf( AtomicBucket::class, $buckets[0] );
+		$this->assertEquals( 'Content', $buckets[0]->data() );
 	}
 }
